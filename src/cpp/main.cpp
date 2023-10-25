13cb164490cb824cf4f5acf888f4d30090a1cc51
Sohib04 <127374092+Sohib04@users.noreply.github.com>
2023-10-25 04:19:37 -1200

    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -16,10 +16,10 @@ int main() {
     int year;
     cout<<"Enter year: ";
     cin>>year;
-    if(year%400 == 0){
+    if(year%400 == 0 && year%100!=0){
         cout<<year<<" is a leap year"<<endl;
     }
-    else if(year%4 == 0){
+    else if(year%4 == 0 && year%100!=0){
         cout<<year<<" is a leap year"<<endl;
     }
     else if(year%100 == 0){
